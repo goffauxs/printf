9e1dc9f6ad7c0b9f5daf8476f01292d2c8cb21a3
Simon Goffaux <sgoffaux@fu-r9-p8.s19.be>
2021-05-21 14:06:19 +0200

    removed unnecessary function

--- srcs/ft_precision.c
+++ srcs/ft_precision.c
@@ -6,26 +6,11 @@
 /*   By: sgoffaux <sgoffaux@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/23 16:02:38 by sgoffaux          #+#    #+#             */
-/*   Updated: 2021/05/04 10:41:38 by sgoffaux         ###   ########.fr       */
+/*   Updated: 2021/05/21 13:38:33 by sgoffaux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-int	ft_get_num_digits(int n)
-{
-	int	i;
-
-	i = 0;
-	if (n == 0)
-		return (1);
-	while (n != 0)
-	{
-		n /= 10;
-		i++;
-	}
-	return (i);
-}
 
 int	ft_precision(const char *format, t_flags *f, va_list args)
 {
